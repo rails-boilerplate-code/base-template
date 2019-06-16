34b9895121f3b94dd1118ada084c1875b8e161f6
Chris Drit <ChrisDrit@gmail.com>
2019-06-15 21:43:25 -0700

    Merged from feature/setup

--- install.rb
+++ install.rb
@@ -10,9 +10,9 @@ after_bundle do
   # DotEnv Gem setup
   matcher = "Bundler.require(*Rails.groups)\n"
   inject_into_file("config/application.rb", after: "#{matcher}") do
-    <<~"BERKELEY"
+    <<~"RUBY"
       Dotenv::Railtie.load
-    BERKELEY
+    RUBY
   end
 
   # ENV stubs and populate
@@ -27,7 +27,6 @@ after_bundle do
 
   stripe_test_secret_key = ask("What is your Stripe Test Secret Key?")
   stripe_test_publish_key = ask("What is your Stripe Test Publishable Key?")
-
   run("echo 'STRIPE_SECRET_KEY=#{stripe_test_secret_key}' >> .env")
   run("echo 'STRIPE_PUBLISHABLE_KEY=#{stripe_test_publish_key}' >> .env")
 
@@ -46,9 +45,9 @@ after_bundle do
 
   matcher = "class ApplicationController < ActionController::Base\n"
   inject_into_file("app/controllers/application_controller.rb", after: "#{matcher}") do
-    <<~"CALIFORNIA"
+    <<~"RUBY"
       before_action :authenticate_user!
-    CALIFORNIA
+    RUBY
   end
 
   # Main site
@@ -57,9 +56,9 @@ after_bundle do
 
   matcher = "class HomeController < ApplicationController\n"
   inject_into_file("app/controllers/home_controller.rb", after: "#{matcher}") do
-    <<~"BEER"
+    <<~"RUBY"
       skip_before_action :authenticate_user!
-    BEER
+    RUBY
   end
 
   # Database
